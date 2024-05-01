ccc2ee01d3d0273960afa2b20da7a4b89e05ed3d
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-05-01 16:03:44 +0300

    ft: create landing page nav

---
+++ landing-page/R/landing_page_nav.R
@@ -0,0 +1,116 @@
+#' Landing page nav
+#'
+#' @param ns Module namespace from which this function is called.
+#' @return [shiny::tags$nav()]
+#' @export
+landing_page_nav <- \(ns) {
+  nav_items <- lapply(
+    X = c("Home", "About", "Features"),
+    FUN = \(nav_item) {
+      class <- paste("nav-link text-white", if (nav_item == "Home") "active")
+      aria_current <- if (nav_item == "Home") "page" else NA
+
+      tags$li(
+        class = "nav-item",
+        actionLink(
+          inputId = tolower(nav_item),
+          label = nav_item,
+          class = class,
+          `aria-current` = aria_current
+        )
+      )
+    }
+  )
+
+  dropdown_items <- lapply(
+    X = c("Hoodies", "Trousers", "Shoes"),
+    FUN = \(dropdown_item) {
+      tagList(
+        tags$li(
+          actionLink(
+            inputId = tolower(dropdown_item),
+            class = "dropdown-item",
+            label = dropdown_item
+          )
+        ),
+        if (dropdown_item == "Trousers") {
+          tags$li(
+            tags$hr(class = "dropdown-divider")
+          )
+        }
+      )
+    }
+  )
+
+  dropdown_menu <- tags$ul(class = "dropdown-menu", dropdown_items)
+
+  tags$nav(
+    class = "navbar bg-dark fixed-top",
+    tags$div(
+      class = "container-fluid",
+      tags$a(
+        class = "navbar-brand text-white",
+        href = "#",
+        "BabyNames"
+      ),
+      tags$button(
+        class = "navbar-toggler",
+        type = "button",
+        `data-bs-toggle` = "offcanvas",
+        `data-bs-target` = paste0("#", ns("offcanvasNavbar")),
+        `aria-controls` = ns("offcanvasNavbar"),
+        `aria-label` = "Toggle navigation",
+        tags$span(
+          class = "navbar-toggler-icon"
+        )
+      ),
+      tags$div(
+        class = "offcanvas offcanvas-end",
+        tabindex = "-1",
+        id = ns("offcanvasNavbar"),
+        `aria-labelledby` = ns("offcanvasNavbarLabel"),
+        tags$div(
+          class = "offcanvas-header",
+          tags$h5(
+            class = "offcanvas-title",
+            id = ns("offcanvasNavbarLabel"),
+            "Menu"
+          ),
+          tags$button(
+            type = "button",
+            class = "btn-close",
+            `data-bs-dismiss` = "offcanvas",
+            `aria-label` = "Close"
+          )
+        ),
+        tags$div(
+          class = "offcanvas-body",
+          tags$ul(
+            class = "navbar-nav justify-content-end align-items-md-center flex-grow-1 pe-3",
+            nav_items,
+            tags$li(
+              class = "nav-item dropdown",
+              tags$a(
+                class = "nav-link dropdown-toggle text-white",
+                href = "#",
+                role = "button",
+                `data-bs-toggle` = "dropdown",
+                `aria-expanded` = "false",
+                "Merchandise"
+              ),
+              dropdown_menu
+            ),
+            tags$li(
+              class = "nav-item",
+              actionButton(
+                inputId = ns("login"),
+                label = "Login",
+                class = "btn-sm btn-primary px-4"
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+}
