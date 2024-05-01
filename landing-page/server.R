15c4c324879e427979a9ece2b7abdefc4d4af0ea
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-05-01 16:03:29 +0300

    ch: initialize shiny app

---
+++ landing-page/server.R
@@ -0,0 +1,3 @@
+server <- \(input, output, session) {
+  mod_landing_page_server(id = "landing_page")
+}
---
+++ landing-page/ui.R
@@ -0,0 +1,6 @@
+ui <- bslib::page(
+  title = "Landing Page",
+  theme = bslib::bs_theme(version = 5, preset = "bootstrap"),
+  lang = "en",
+  mod_landing_page_ui(id = "landing_page")
+)
