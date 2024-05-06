41c0dc8cc344b7f0fb71752b0d01ebfada92b57e
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-05-06 22:35:56 +0300

    ch: use `box::use()` for imports

--- landing-page/ui.R
+++ landing-page/ui.R
@@ -1,13 +1,14 @@
 box::use(
+  bslib[page, bs_theme],
   . / landing_page / ui[mod_landing_page_ui = ui]
 )
 
 #' App UI
 #'
 #' @export
-ui <- bslib::page(
+ui <- page(
   title = "Landing Page",
-  theme = bslib::bs_theme(version = 5, preset = "bootstrap"),
+  theme = bs_theme(version = 5, preset = "bootstrap"),
   lang = "en",
   tags$link(
     rel = "stylesheet",
